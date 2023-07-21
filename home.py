6557d6920149b432f543ce380def438a4bbf8154
Ila Esmaeili <87114919+ilaesm@users.noreply.github.com>
2023-07-20 23:37:00 -0700

    update

--- home.py
+++ home.py
@@ -3,7 +3,10 @@ import yfinance as yf
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-from datetime import datetime
+from datetime import datetime, timezone  # Import timezone from datetime module
+import pyfolio as pf
+import warnings
+warnings.filterwarnings('ignore')
 
 def main():
     st.title("Portfolio Dashboard")
@@ -15,10 +18,10 @@ def main():
     open_date = st.sidebar.date_input("Position Purchase Date")
     close_date = st.sidebar.date_input("Position Close (current date is default)")
     industry_select = st.sidebar.selectbox('Select Industry', dfEquity)
-
     add_asset_button = st.sidebar.button("Add Asset")
     undo_button = st.sidebar.button("Undo Asset")
 
+    # Initialize the asset_list in session state
     if 'asset_list' not in st.session_state:
         st.session_state.asset_list = []
 
@@ -32,38 +35,48 @@ def main():
 
     st.table(st.session_state.asset_list)
 
-    # Create a dataframe to hold historical price data for all assets in the portfolio
-    price_data = pd.DataFrame()
+    if st.session_state.asset_list:
+        price_data = pd.DataFrame()
 
-    # Loop over all assets in the portfolio
-    for asset in st.session_state.asset_list:
-        # Fetch historical price data for the asset
-        stock_data = yf.download(asset["Ticker"], start=asset["Position Open"], end=asset["Position Close"])
-        # Calculate the asset value over time and add it to the price data dataframe
-        if asset["Type"] == "Buy":
-            price_data[asset["Ticker"]] = stock_data['Close'] * asset["Amount of Shares"]
-        else:  # asset is shorted
-            price_data[asset["Ticker"]] = (stock_data['Close'] * asset["Amount of Shares"])
-    # Calculate the portfolio value over time
-    # Calculate the portfolio ROI by summing ROIs of individual assets
-    portfolio_value = price_data.sum(axis=1)
-    total_value_added = portfolio_value.iloc[-1] + starting_cash
-    delta_value = ((st.session_state.last_total_value / starting_cash) * 100)
-    st.session_state.last_total_value = total_value_added
+        for asset in st.session_state.asset_list:
+            # Convert open_date and close_date to datetime objects and set them to UTC timezone
+            open_date_utc = datetime(asset["Position Open"].year, asset["Position Open"].month, asset["Position Open"].day, tzinfo=timezone.utc)
+            close_date_utc = datetime(asset["Position Close"].year, asset["Position Close"].month, asset["Position Close"].day, tzinfo=timezone.utc)
 
+            stock_data = yf.download(asset["Ticker"], start=open_date_utc, end=close_date_utc)
+            if not stock_data.empty:
+                if asset["Type"] == "Buy":
+                    price_data[asset["Ticker"]] = stock_data['Close'] * asset["Amount of Shares"]
+                else:
+                    price_data[asset["Ticker"]] = (stock_data['Close'] * asset["Amount of Shares"]) * -1
 
+        if not price_data.empty:
+            portfolio_value = price_data.sum(axis=1)
+            if not portfolio_value.empty:
+                total_value_added = portfolio_value.iloc[-1] + starting_cash
+                if 'last_total_value' in st.session_state:
+                    delta_value = ((total_value_added / starting_cash) - 1) * 100
+                else:
+                    delta_value = 0 
+                st.session_state.last_total_value = total_value_added
 
-    # Create a plotly line graph for portfolio value
-    fig = px.line(portfolio_value, title='Portfolio Historical Performance')
-    fig.update_layout(
-    autosize=True,
-    width=700,
-    height=600,
-)
-    st.plotly_chart(fig)
-    st.metric(label="Current Portfolio Value", 
-            value= f'${total_value_added:,.2f}', 
-            delta= f'{delta_value:,.2f}%')
+                fig = px.line(portfolio_value, title='Portfolio Historical Performance')
+                fig.update_layout(
+                    autosize=True,
+                    width=700,
+                    height=600,
+                )
+                st.plotly_chart(fig)
+                st.metric(label="Current Portfolio Value", 
+                          value= f'${total_value_added:,.2f}', 
+                          delta= f'{delta_value:,.2f}%')
+
+                latest_values = price_data.iloc[-1]
+                fig = px.pie(values=latest_values, names=latest_values.index, title='Portfolio Allocation')
+                fig.update_layout(autosize=True, width=900, height=700)
+                st.plotly_chart(fig)
+    else:
+        st.warning("Please add at least one asset to the portfolio.")
 
 if __name__ == "__main__":
     main()
--- logic.py
+++ logic.py
@@ -10,9 +10,13 @@ dfEquity = pd.DataFrame(equities_industry_groups)
 
 #pie chart weighting logic
 
+fb = yf.Ticker('FB')
+history = fb.history('max')
+history.index = history.index.tz_localize('utc')
+history.info()
 
 
-
+pf.create_returns_tear_sheet(returns, live_start_date='2020-1-1')
 
 
 
